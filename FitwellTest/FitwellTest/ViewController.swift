e260e7d4582c4ebf5a369b4c78034c3833dd2d26
Cemal Bayrı <cemal.bayri@dreamsandbytes.com>
2018-07-06 23:25:38 +0300

    first commit

---
+++ FitwellTest/FitwellTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitwellTest
+//
+//  Created by Erencan Evren on 6.07.2018.
+//  Copyright © 2018 Cemal BAYRI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
